d69fd5d6eeb667c8b82a81b2fff0517910d803ec
Nilesoj20 <jvrn2014@gmail.com>
2023-08-28 22:11:00 -0500

    feat: function that returns a concatenated string of 2 parameters str

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+""" function notes str1 and str2 as arguments and returns a string """
+
+
+def concat(str1: str, str2: str) -> str:
+    """ function returns a string
+        Args: 
+            str1(str):  first string
+            srt2(str): second string
+        Return:
+            a concatenated chain
+    """
+    return str1 + str2
