f8d54e96b61c1f4d03f8d2ad44c9b5ba0a8c473f
Nilesoj20 <jvrn2014@gmail.com>
2023-08-31 17:54:06 -0500

    fix: applying the concept of annotation variables

--- python_async_function/0-basic_async_syntax.py
+++ python_async_function/0-basic_async_syntax.py
@@ -4,8 +4,8 @@ import random
 import asyncio
 
 
-async def wait_random(max_delay=10):
+async def wait_random(max_delay: int = 10) -> float:
     """  function that expects a random delay """
-    aleatorio = random.uniform(0, max_delay)
+    aleatorio: float = random.uniform(0, max_delay)
     await asyncio.sleep(aleatorio)
     return aleatorio
