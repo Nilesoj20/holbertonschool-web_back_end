e07a575bf1b4c9cd9b3475aea6bf50d8826abe67
Nilesoj20 <jvrn2014@gmail.com>
2023-09-01 20:38:55 -0500

    fix: correcting the return type

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
 """ Asynchronous compressions """
-from typing import Generator
+import typing
 
 async_generator = __import__('0-async_generator').async_generator
 
 
-async def async_comprehension() -> Generator[float, None, None]:
+async def async_comprehension() -> typing.List[float]:
     """ corutina will collect 10 random numbers """
     async_num = [num async for num in async_generator()]
     return async_num
