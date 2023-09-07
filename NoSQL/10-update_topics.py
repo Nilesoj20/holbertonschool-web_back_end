ed5536ba5c9b001fdff3990e12a8256e704e2f2c
Nilesoj20 <jvrn2014@gmail.com>
2023-09-07 13:59:33 -0700

    feat: change all topics in a document

---
+++ NoSQL/10-update_topics.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+""" Change school subjects """
+
+
+def update_topics(mongo_collection, name, topics):
+    """ change all topics in a document """
+    filtro = {"name": name}
+    remplazo = {
+            "$set": {"topics": topics}}
+    actualizacion = mongo_collection.update_one(filtro, remplazo)
