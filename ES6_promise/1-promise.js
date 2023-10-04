a71ae8bb986b9ac394336ade726def83b332263a
Nilesoj20 <jvrn2014@gmail.com>
2023-10-04 20:55:20 +0000

    feat: Don't make a promise...if you know you can't keep it

---
+++ ES6_promise/1-promise.js
@@ -0,0 +1,11 @@
+/* promise by passing an object with 2 attributes*/
+export default function getFullResponseFromAPI(success) {
+  const pro = new Promise((resolve, reject) => {
+    if (success) {
+      resolve({ status: 200, body: 'Success' });
+    } else {
+      reject(new Error('The fake API is not working currently'));
+    }
+  });
+  return arr;
+}
