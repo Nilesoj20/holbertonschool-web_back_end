d91f8edc11b6db8ba5d4dbb38eae3c677a396c3e
Nilesoj20 <jvrn2014@gmail.com>
2023-10-08 20:25:22 +0000

    feat: function updateStudentGradeByCity that returns an array of students for a specific city with their new grade

---
+++ ES6_data_manipulation/4-update_grade_by_city.js
@@ -0,0 +1,11 @@
+/* function that returns a variety of students for a specific city with their new grade */
+export default function updateStudentGradeByCity(students, city, newGrades) {
+  if (!Array.isArray(students) || !Array.isArray(newGrades)) {
+    return [];
+  }
+  const cityStudents = students.filter((student) => student.location === city).map((student) => {
+    const item2 = newGrades.find((student2) => student.id === student2.studentId);
+    return { ...student, grade: item2 ? item2.grade : 'N/A' };
+  });
+  return cityStudents;
+}
