5c34b40d8a147ce7f88568b3cadb96a49b4fdb9c
Nilesoj20 <jvrn2014@gmail.com>
2023-10-08 20:39:03 +0000

    feat: Clean set

---
+++ ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,12 @@
+/* function that returns a string of all set values starting with a specific string */
+const cleanSet = (set, startString) => {
+  if (startString === undefined || startString.length === 0) {
+    return '';
+  }
+  return [...set]
+    .filter((parametro) => (parametro !== undefined ? parametro.startsWith(startString) : ''))
+    .map((parametro) => (parametro !== undefined ? parametro.slice(startString.length) : ''))
+    .join('-');
+};
+
+export default cleanSet;
